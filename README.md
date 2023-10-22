1e420e06b8ed47762c83179b3f40732d550ea554
Mihnea Simian <contact@mesimian.com>
2023-10-22 16:20:56 +0300

    initial scaffolding

---
+++ README.md
@@ -0,0 +1,8 @@
+## Open-wc Starter App
+
+[![Built with open-wc recommendations](https://img.shields.io/badge/built%20with-open--wc-blue.svg)](https://github.com/open-wc)
+
+## About
+
+Small pet project to experiment with lit.
+Helps counting working days for in a given month. Nothing a spreadsheet can't handle.
---
+++ src/days-counter-app.ts
@@ -0,0 +1,57 @@
+import { LitElement, html, css } from 'lit';
+import { property, customElement } from 'lit/decorators.js';
+
+@customElement('days-counter-app')
+export class DaysCounterApp extends LitElement {
+  @property({ type: Number }) workingDaysOfMonth = 21;
+
+  static styles = css`
+    :host {
+      min-height: 100vh;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: flex-start;
+      font-size: calc(10px + 2vmin);
+      color: #1a2b42;
+      max-width: 960px;
+      margin: 0 auto;
+      text-align: center;
+      background-color: var(--days-counter-app-background-color);
+    }
+
+    main {
+      flex-grow: 1;
+    }
+
+    .app-footer {
+      font-size: calc(12px + 0.5vmin);
+      align-items: center;
+    }
+
+    .app-footer a {
+      margin-left: 5px;
+    }
+  `;
+
+  render() {
+    return html`
+      <main>
+        <h1>Counting workdays</h1>
+
+        <div>Total Working Days: ${this.workingDaysOfMonth}</div>
+
+      </main>
+
+      <p class="app-footer">
+        🚽 Made with love by
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/open-wc"
+          >open-wc</a
+        >.
+      </p>
+    `;
+  }
+}
